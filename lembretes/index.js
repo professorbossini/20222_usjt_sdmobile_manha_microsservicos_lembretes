f1ec2291bb881526331512da01eaf302467925f5
Rodrigo Bossini <professorbossini@gmail.com>
2022-09-12 15:30:46 -0300

    microsservicos(apostila03): mss lembretes
    - endpoint GET implementado
    - endpoint POST implementado

---
+++ lembretes/index.js
@@ -0,0 +1,43 @@
+const express = require ('express')
+const app = express()
+//converte o corpo da requisição que era puro texto
+//para um objeto JSON
+app.use(express.json())
+const lembretes = {}
+//identificador de lembretes
+//a cada lembrete novo ele é incrementado
+contador = 0
+
+
+//armazenar um novo lembrete
+//POST localhost:4000/lembretes {texto: "Fazer café"}
+//endpoint para armazenamento de lembretes
+/*a base é assim:
+  {
+    1: {
+      contador: 1, texto: 'Fazer café'
+    },
+    2: {
+      contador: 2, texto: 'Natação'
+    }
+  }
+*/
+app.post('/lembretes', (req, res) => {
+  contador++
+  const texto = req.body.texto
+  lembretes[contador] = {contador, texto}
+  //201 significa "created"
+  res.status(201).send(lembretes[contador])
+})
+
+//obter a lista completa de lembretes
+//GET localhost:4000/lembretes
+//endpoint para obtenção da lista de lembretes
+app.get('/lembretes', (req, res) => {
+  //não pense em usar return
+  console.log('executando...')
+  res.send(lembretes)
+
+})
+
+app.listen(4000, () => console.log("Lembretes. Porta 4000."))
